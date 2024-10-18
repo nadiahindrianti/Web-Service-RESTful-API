f6eef43f0c86ab3b6e1fc505ec626af61cbce197
nadiahindrianti <104046022+nadiahindrianti@users.noreply.github.com>
2024-10-18 23:43:41 +0700

    undo

--- config/config.js
+++ config/config.js
@@ -3,13 +3,23 @@ require('dotenv').config();
 const env = process.env.NODE_ENV || 'development';
 
 const config = {
-  production: {
-    use_env_variable: 'DATABASE_URL',
+  development: {
+    username: process.env.MYSQL_USER || 'root',
+    password: process.env.MYSQL_PASSWORD || 'Nadiah271,.',  
+    database: process.env.MYSQL_DATABASE || 'todo_db',
+    host: process.env.MYSQL_HOST || 'localhost',
     dialect: 'mysql',
-    logging: false
+    port: process.env.MYSQL_PORT || 3306,
+    logging: console.log  
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL',  
+    dialect: 'mysql',
+    logging: false  
   }
 };
 
+console.log('MySQL User:', process.env.MYSQL_USER);
+console.log('MySQL Host:', process.env.MYSQL_HOST);
 
 module.exports = config;
-
