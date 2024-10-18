ab9e823d99da89231338159e62c77f16b7c700a4
nadiahindrianti <104046022+nadiahindrianti@users.noreply.github.com>
2024-10-18 23:38:51 +0700

    remove config development

--- config/config.js
+++ config/config.js
@@ -3,24 +3,13 @@ require('dotenv').config();
 const env = process.env.NODE_ENV || 'development';
 
 const config = {
-  development: {
-    username: process.env.MYSQL_USER || 'root',
-    password: process.env.MYSQL_PASSWORD || 'Nadiah271,.',  
-    database: process.env.MYSQL_DATABASE || 'todo_db',
-    host: process.env.MYSQL_HOST || 'localhost',
-    dialect: 'mysql',
-    port: process.env.MYSQL_PORT || 3306,
-    logging: console.log  
-  },
   production: {
-    use_env_variable: 'DATABASE_URL',  
+    use_env_variable: 'DATABASE_URL',
     dialect: 'mysql',
-    logging: false  
+    logging: false
   }
 };
 
-console.log('MySQL User:', process.env.MYSQL_USER);
-console.log('MySQL Host:', process.env.MYSQL_HOST);
 
 module.exports = config;
 
